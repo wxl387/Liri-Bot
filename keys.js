e25a65025e43cc9b0a6cc778f7984513d60ef887
Wenbiao Li <lixuyao.tony@gmail.com>
2017-09-25 13:48:48 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'HbvIVbRzx3zHUex5S7gPgEcby',
+  consumer_secret: '	d3qJtO4VvIhThKYmrWmMieG3ZeXHzcNMu4cjMrt7aM5Yc99qKf',
+  access_token_key: '910610159818412032-JH3fKkazsW57Hmmomu7DXvgTXK8h5xw',
+  access_token_secret: '	iMgg0uuWW9EHdzNDVh4elhYXWesMx1ZiguM1XfG6OHWo0',
+}
+
+var spotifyKeys = {
+	id: 'd46b78079e324fe58d2d1d951cfa7622',
+	secret: 'acad0e14af724367a5e39db51f56c2ca',
+}
+module.exports.twitterKeys = twitterKeys;
+module.exports.spotifyKeys = spotifyKeys;
